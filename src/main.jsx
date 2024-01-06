90028a6987a2342f9adc13988b453957c40c48d6
ovoxiix <ovoxiix@gmail.com>
2024-01-06 23:45:02 +0900

    🚧 Add an index route

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import Root, {
   action as rootAction,
 } from "./routes/root";
 import ErrorPage from "./error-page";
+import Index from "./routes/index";
 import Contact, { loader as contactLoader } from "./routes/contact";
 import EditContact, { action as editAction } from "./routes/edit";
 import { action as destroyAction } from "./routes/destroy";
@@ -19,6 +20,7 @@ const router = createBrowserRouter([
     loader: rootLoader,
     action: rootAction,
     children: [
+      { index: true, element: <Index /> },
       {
         path: "contacts/:contactId",
         element: <Contact />,
